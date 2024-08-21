f3ca5db16237361ac13cd07f41e297ffd454b4a8
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-08-21 16:51:40 +0200

    simple transition with ease on opacity

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {ref} from "vue";
+
+const showItems = ref(false)
+
+const items = ref([
+  {key: 1, value: 'one'},
+  {key: 2, value: 'two'},
+  {key: 3, value: 'three'},
+]);
+
+</script>
+
+<template>
+  <button @click="showItems = !showItems"
+          style="margin: 2px; padding: 8px; background-color: rgba(200,0,89,0.2) ; border-radius: 5px;">
+    {{ showItems ? 'Hide' : 'Show' }} Items
+  </button>
+  <transition>
+    <ul v-if="showItems">
+      <li v-for="item in items" :key="item.key"> {{ item.value }}</li>
+    </ul>
+  </transition>
+</template>
+
+<style scoped>
+
+</style>
