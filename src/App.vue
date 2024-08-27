2e3b614e5ded544c62ed029cf96d74aec08b961c
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-08-27 09:48:45 +0200

    applying duration on Transitions

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import {ref} from "vue";
+
 const showModal = ref(false);
 
 </script>
@@ -10,32 +11,57 @@ const showModal = ref(false);
     Show Modal
   </button>
 
-  <div v-if="showModal" class="modal-wrapper modal-bg">
-    <div class="modal">
-      <p> Hello, I am modal</p>
-    </div>
-  </div>
+ <Transition :duration="1400">
+   <div v-if="showModal" class="modal-wrapper">
+     <div class="modal-bg" @click="showModal = false"></div>
+     <div class="modal">
+       <p> Hello, I am modal</p>
+     </div>
+   </div>
+ </Transition>
 
 </template>
 
 <style scoped>
-.modal-wrapper {
+.modal-wrapper .modal-bg {
   position: absolute;
   top: 0;
   bottom: 0;
   right: 0;
   left: 0;
+  display: flex;
   justify-content: center;
   align-items: center;
 }
-.modal-bg{
-  background-color: rgba(0,0,0, 0.4);
+
+.modal-bg {
+  background-color: rgba(0, 0, 0, 0.4);
 }
-.modal{
+
+.modal {
   background-color: white;
   position: relative;
   z-index: 2;
   padding: 5px;
   border-radius: 2px;
+}
+
+.modal .modal-bg{
+  transition: all 2s ease;
+}
+.v-enter-active .modal{
+  transition-delay: 0.4s;
+}
+.v-leave-active .modal-bg{
+  transition-delay: 0.4s;
+}
+.v-enter-from .modal-bg,
+.v-leave-to .modal-bg{
+  opacity: 0;
+}
+.v-enter-from .modal,
+.v-leave-to .modal{
+  opacity: 0;
+  transform: translateX(-30px);
 }
 </style>
