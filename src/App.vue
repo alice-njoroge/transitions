5e4cc8cbff647d02a9a35066b49577ced7bed4a1
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-08-27 08:50:23 +0200

    modal - absolute and relative css positions, z-index- layered components

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,41 @@
 <script setup>
 import {ref} from "vue";
-
-const showItems = ref(false)
-
-const items = ref([
-  {key: 1, value: 'one'},
-  {key: 2, value: 'two'},
-  {key: 3, value: 'three'},
-]);
+const showModal = ref(false);
 
 </script>
 
 <template>
-  <button @click="showItems = !showItems"
-          style="margin: 2px; padding: 8px; background-color: rgba(200,0,89,0.2) ; border-radius: 5px;">
-    {{ showItems ? 'Hide' : 'Show' }} Items
+  <button @click="showModal = !showModal"
+          style="margin: 2px; padding: 8px; background-color: rgba(55,2,2,0.2) ; border-radius: 5px;">
+    Show Modal
   </button>
-  <transition name="fade">
-    <ul v-if="showItems">
-      <li v-for="item in items" :key="item.key"> {{ item.value }}</li>
-    </ul>
-  </transition>
+
+  <div v-if="showModal" class="modal-wrapper modal-bg">
+    <div class="modal">
+      <p> Hello, I am modal</p>
+    </div>
+  </div>
+
 </template>
 
 <style scoped>
-
-.fade-enter-active,
-.fade-leave-active{
-  transition: opacity 5s ease, transform 5s ease;
-} 
-
-.fade-enter-from,
-.fade-leave-to{
-  opacity: 0;
-  transform: translateY(-10px)
+.modal-wrapper {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  justify-content: center;
+  align-items: center;
 }
-
+.modal-bg{
+  background-color: rgba(0,0,0, 0.4);
+}
+.modal{
+  background-color: white;
+  position: relative;
+  z-index: 2;
+  padding: 5px;
+  border-radius: 2px;
+}
 </style>
